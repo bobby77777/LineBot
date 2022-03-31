aaeec0de4bcc3cf23f1ddfb0978f923c6180fe07
bobby77777 <bobby7892143@gmail.com>
2022-03-31 22:44:48 +0800

    second step

---
+++ app.py
@@ -0,0 +1,40 @@
+import json
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImagemapSendMessage
+
+with open("/Users/willy/Downloads/line_bot/channel.json") as f:
+    channel = json.load(f)
+app = Flask(__name__)
+
+# Channel
+len_bot_api = LineBotApi(channel['token'])
+handler = WebhookHandler(channel['secret'])
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
