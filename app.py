6f29c063ee038ec071748a8d906ac6927aa91b6d
willy <abc9450208@gmail.com>
2022-07-05 15:07:55 +0800

    first init

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from boto.s3.connection import S3Connection
 
 s3 = S3Connection(os.environ["channel_access_token"], os.environ['channel_secret'])
 print('-------------------------')
-print(s3)
+print(s3,type(s3))
 print('-------------------------')
 # # LINE 聊天機器人的基本資料
 # config = configparser.ConfigParser()
