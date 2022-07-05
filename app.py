8450dce455cd1ddaaddea8c8cb233406f36e352c
willy <abc9450208@gmail.com>
2022-07-05 15:40:24 +0800

    first init

--- app.py
+++ app.py
@@ -36,6 +36,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # event->使用者資料
+    print('-----------------------')
+    print(type(event))
+    print(event)
+    print('-----------------------')
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
