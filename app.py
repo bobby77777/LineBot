61dbf26b69982b674cf6cef1f851835064c71e68
willy <abc9450208@gmail.com>
2022-07-25 23:07:41 +0800

    postback handle

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # event->使用者資料
-    userSend = event.message.text
+    # userSend = event.message.text
     print('======================')
     print(event.message)
     print('======================')
