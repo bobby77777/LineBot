da65498b969936649b8a8104511242b49b258dae
willy <abc9450208@gmail.com>
2022-07-26 09:32:04 +0800

    postback handle

--- app.py
+++ app.py
@@ -42,85 +42,77 @@ def callback():
     
 
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent)
 def handle_message(event):
     # event->使用者資料
-    userSend = event.message.text
-    print('======================')
-    print(userSend)
-    print('======================')
-    userSend = [item for item in userSend.split(' ') if item != '']
-    Date = str(date.today()).replace('-','')
-    if userSend[0].isdigit():
-        if len(userSend) > 1:
-            # userSend[0], data[0] -> symbol, 中文名
-            pre = int(userSend[1])
-            data = Get_StockPrice(userSend[0], pre,Date)
-        else:
-            pre = 20
-            data = Get_StockPrice(userSend[0])
-        print('======================')
-        print(data)
-        print(type(data))
-        print('======================')
-        if type(data) == type('incorrect'):
-            message = TextSendMessage(text='請輸入正確的股票代號')
-            
-        else:
-            # data[0] -> symbol
-            while len(data[1]) < int(pre):
-                month = str(int(Date[4:6])-1)
-                if month == '0':
-                    Date = str(int(Date[0:4])-1)+'1201'
-                else:
-                    month = '0'+month if len(month) < 2 else month
-                    Date = Date[0:4]+month+'01'
-                data[1] = pd.concat([Get_StockPrice(userSend[0],  str(pre-len(data[1])), Date)[1],data[1]])
-            # TEXT message
-            info = data[0]+'\n----------------'
-            d = data[1].values[-1]
-            info += '\n{}\n收盤:{}\n開盤:{}\n最高價:{}\n最低價:{}\n交易量(張):{}\n'\
-                .format(d[0].date(), d[4], d[1], d[2], d[3], d[5])
-            info = info[:-1]
-            message = TextSendMessage(text=info)
-            # Image message
-            stock_graph(userSend[0], data[1])
-            path = "./send.png"
-            uploaded_image = im.upload_image(path, title="Uploaded with PyImgur")
-            image_message = ImageSendMessage(original_content_url=uploaded_image.link,\
-                                            preview_image_url=uploaded_image.link)
-            reply_arr = [message, image_message]
-            line_bot_api.reply_message(event.reply_token, reply_arr)
-            return 0
+    if event.message.type != 'text':
+        message = TextSendMessage(text='你可以傳個股票代碼試試')
     else:
-        if userSend[0] == '匯率':
-            bubble_image = im.get_image('x35HtQC').link
-            currency_bubble["hero"]["url"] = bubble_image
-            message = FlexSendMessage(alt_text='請選擇幣別', contents=currency_bubble)
-            print('-------------------')
-            print(message)
-            print('-------------------')
+        userSend = event.message.text
+        print('======================')
+        print(userSend)
+        print('======================')
+        userSend = [item for item in userSend.split(' ') if item != '']
+        Date = str(date.today()).replace('-','')
+        if userSend[0].isdigit():
+            if len(userSend) > 1:
+                # userSend[0], data[0] -> symbol, 中文名
+                pre = int(userSend[1])
+                data = Get_StockPrice(userSend[0], pre,Date)
+            else:
+                pre = 20
+                data = Get_StockPrice(userSend[0])
+            print('======================')
+            print(data)
+            print(type(data))
+            print('======================')
+            if type(data) == type('incorrect'):
+                message = TextSendMessage(text='請輸入正確的股票代號')
+                
+            else:
+                # data[0] -> symbol
+                while len(data[1]) < int(pre):
+                    month = str(int(Date[4:6])-1)
+                    if month == '0':
+                        Date = str(int(Date[0:4])-1)+'1201'
+                    else:
+                        month = '0'+month if len(month) < 2 else month
+                        Date = Date[0:4]+month+'01'
+                    data[1] = pd.concat([Get_StockPrice(userSend[0],  str(pre-len(data[1])), Date)[1],data[1]])
+                # TEXT message
+                info = data[0]+'\n----------------'
+                d = data[1].values[-1]
+                info += '\n{}\n收盤:{}\n開盤:{}\n最高價:{}\n最低價:{}\n交易量(張):{}\n'\
+                    .format(d[0].date(), d[4], d[1], d[2], d[3], d[5])
+                info = info[:-1]
+                message = TextSendMessage(text=info)
+                # Image message
+                stock_graph(userSend[0], data[1])
+                path = "./send.png"
+                uploaded_image = im.upload_image(path, title="Uploaded with PyImgur")
+                image_message = ImageSendMessage(original_content_url=uploaded_image.link,\
+                                                preview_image_url=uploaded_image.link)
+                reply_arr = [message, image_message]
+                line_bot_api.reply_message(event.reply_token, reply_arr)
+                return 0
         else:
-            message = TextSendMessage(text='你可以傳個股票代碼試試')
+            if userSend[0] == '匯率':
+                bubble_image = im.get_image('x35HtQC').link
+                currency_bubble["hero"]["url"] = bubble_image
+                message = FlexSendMessage(alt_text='請選擇幣別', contents=currency_bubble)
+                
+            else:
+                message = TextSendMessage(text='你可以傳個股票代碼試試')
 
     line_bot_api.reply_message(event.reply_token, message)
     return 0
-@handler.add(MessageEvent)
+@handler.add(PostbackEvent)
 def handle_message(event):
-    print(event.type)
-    if event.type == 'message' and event.message.type != 'text':
-        message = TextSendMessage(text='你可以傳個股票代碼試試')
-
-    elif event.type == 'postback':
-        print('------------')
-        print(type(event.postback.data))
-        print(event.postback.data)
-        print('------------')
-        get_halfyear(event.postback.data)
-        path = "./currency.png"
-        uploaded_image = im.upload_image(path, title="Uploaded with PyImgur")
-        message = ImageSendMessage(original_content_url=uploaded_image.link,\
-                                        preview_image_url=uploaded_image.link)
+    get_halfyear(event.postback.data)
+    path = "./currency.png"
+    uploaded_image = im.upload_image(path, title="Uploaded with PyImgur")
+    message = ImageSendMessage(original_content_url=uploaded_image.link,\
+                            preview_image_url=uploaded_image.link)
     line_bot_api.reply_message(event.reply_token, message)
     return 0
 
