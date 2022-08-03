dfe6ab4a7ac23fd17d765c2c759b0f74549c28f4
bobby77777 <bobby7892143@gmail.com>
2022-08-03 21:58:07 +0800

    Update stock.py

--- python_file/stock.py
+++ python_file/stock.py
@@ -24,7 +24,7 @@ def Get_StockPrice(Symbol, previousDay=1, Date=str(date.today()).replace('-','')
     data = requests.get(url,headers=headers).text
     json_data = json.loads(data)
     try:
-        stock_name = [item for item in json_data["title"].split(' ') if item != ''][-2]
+        stock_code = [item for item in json_data["title"].split(' ') if item != ''][-2]
         Stock_data = json_data['data']
         StockPrice = pd.DataFrame(Stock_data, columns = ['Date','Volume','Volume_Cash','Open','High','Low','Close','Change','Order'])
         StockPrice['Date'] = StockPrice['Date'].str.replace('/','').astype(int) + 19110000
@@ -43,17 +43,18 @@ def Get_StockPrice(Symbol, previousDay=1, Date=str(date.today()).replace('-','')
         # print(StockPrice[-previousDay: :])
         # if len(Date) < 8:
         # print(StockPrice.loc[Date[:4]+'-'+Date[4:6]+'-'+Date[-2:]:])
-        return [stock_name, StockPrice[-int(previousDay): :]]
+        return [stock_code, StockPrice[-int(previousDay): :]]
     except:
         return 'incorrect'
 
-def stock_graph(stock_name, data):
+def stock_graph(stock_name, stock_code, data):
     plt.rcparams['font.Sans-serif'] = ['Simhei']
     plt.rcParams['axes.unicode_minus '] = False
     data = data.set_index('Date', drop = True)
     mc = mpf.make_marketcolors(up='r', down='g', inherit=True)
     s = mpf.make_mpf_style(base_mpf_style='yahoo', marketcolors=mc, rc={'font.family': 'Simhei'})
-    # kwargs = dict(type='candle', mav=(5,20), volume=True, panel_ratios=(3,1), figratio=(20,10), figscale=0.75, title='\n\n'+stock_name, style=s)
+    # kwargs = dict(type='candle', mav=(5,20), volume=True, panel_ratios=(3,1), figratio=(20,10), figscale=0.75, title='\n\n'+stock_code, style=s)
+    # kwargs = dict(type='candle', mav=(5,20), volume=True, title='\n\n'+stock_code, style=s)
     kwargs = dict(type='candle', mav=(5,20), volume=True, title='\n\n'+stock_name, style=s)
     mpf.plot(data, **kwargs,savefig='./photos/send.png')
     
